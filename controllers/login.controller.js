3b85d337ca60dc353cf43641bad402d2daf03d6f
Luis <lamadoalvarez@gmail.com>
2025-03-31 13:18:28 -0600

    added salt and pepper hashing for passwords

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,16 +1,53 @@
 import { sqlConnect, sql } from '../utils/sql.js';
+import crypto from 'crypto';
 
 export const login = async (req, res) => {
-  console.log(req.body);
+  const { username, password } = req.body;
+
   const pool = await sqlConnect();
   const data = await pool
     .request()
-    .input('username', sql.VarChar, req.body.username)
+    .input('username', sql.VarChar, username)
     .query('SELECT * FROM users WHERE username=@username');
-  const isLogin = data.recordset[0]?.password === req.body.password;
+
+  const storedPassword = data.recordset[0]?.password;
+  const salt = storedPassword.slice(0, 9);
+  console.log(salt);
+
+  const hashing = crypto.createHash('sha512');
+  const hash = hashing
+    .update(salt + process.env.PEPPER + password)
+    .digest('base64url');
+
+  const isLogin = storedPassword === salt + hash;
   if (isLogin) {
     res.status(200).json({ isLogin, user: data.recordset[0] });
   } else {
     res.status(400).json({ isLogin, user: null });
   }
+};
+
+export const register = async (req, res) => {
+  const { username, password, name } = req.body;
+  const salt = crypto.randomBytes(24).toString('base64url').slice(0, 9);
+  console.log(salt);
+  const pepperPassword = process.env.PEPPER + password;
+  const saltedPassword = salt + pepperPassword;
+
+  const hashing = crypto.createHash('sha512');
+  const hash = hashing.update(saltedPassword).digest('base64url');
+
+  const storedPassword = salt + hash;
+
+  const pool = await sqlConnect();
+  await pool
+    .request()
+    .input('username', sql.VarChar, username)
+    .input('password', sql.VarChar, storedPassword)
+    .input('name', sql.VarChar, name)
+    .query(
+      'INSERT INTO users (name, username, password) VALUES (@name, @username, @password)'
+    );
+
+  res.status(200).json({ message: 'Created user' });
 };
--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { login } from '../controllers/login.controller.js';
+import { login, register } from '../controllers/login.controller.js';
 
 const router = Router();
 
 router.post('/login', login);
+router.post('/register', register);
 
 export default router;
